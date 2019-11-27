452af0c408a5036f2fa0eef87fec6199d59ba84f
alinemtg <amtg@cin.ufpe.br>
2019-11-27 20:43:25 -0300

    some refactor in dango class to prevent 'fenotype' recalculation

--- dango.js
+++ dango.js
@@ -10,6 +10,19 @@ class Dango {
     this.y = random(0.8*windowHeight)
 
     this.colorBox = new Rectangle(this.index*windowWidth/20, 0.87*windowHeight,  windowWidth/20, 0.045*windowHeight);
+
+    // ~ we are using the dango's DNA to pick properties for it ~
+
+    this.bodyColor = color(this.dna.genes[1], this.dna.genes[2], this.dna.genes[3]) // this.dna.genes[3] is the one related to brightness
+    this.eyecolor = color(this.dna.genes[4], this.dna.genes[5], this.dna.genes[6])
+    this.cheeksColor = color(this.dna.genes[7], this.dna.genes[8], this.dna.genes[9])
+    
+    this.body_scale = map(this.dna.genes[0], 0, 1, 0.15, 0.75)
+    this.eye_scale = map(this.dna.genes[10], 0, 1, 0.35*this.body_scale, 0.95*this.body_scale)
+    this.cheeks_scale = map(this.dna.genes[10], 0, 1, 0.35*this.body_scale, 0.95*this.body_scale)
+
+    this.eyes_type = floor(map(this.dna.genes[11], 0, 1, 0, 3))
+    this.cheeks_type = floor(map(this.dna.genes[12], 0, 1, 0, 2))
   }
 
 
@@ -18,10 +31,7 @@ class Dango {
   // ='='='='='='='='= HOUR INFLUENCE ='='='='='='='='=
 
   getHourInfluence(){
-    let genes = this.dna.genes;
-    let eyes_type = floor(map(genes[11], 0, 1, 0, 3))
-    let cheeks_type = floor(map(genes[12], 0, 1, 0, 2))
-    this.fitness += day.getHourInfluence(genes[3], eyes_type, cheeks_type)
+    this.fitness += day.getHourInfluence(this.dna.genes[3], this.eyes_type, this.cheeks_type)
   }
 
   // ='='='='='='='='= USER INFLUENCE ='='='='='='='='=
@@ -41,33 +51,16 @@ class Dango {
   // ='='='='='='='='= THE DANGO ITSELF ='='='='='='='='=
 
   display() {
-
-    // ~ we are using the dango's DNA to pick properties for it ~
-
-    let genes = this.dna.genes
-
-    let bodyColor = color(genes[1], genes[2], genes[3]) // genes[3] is the one related to brightness
-    let eyecolor = color(genes[4], genes[5], genes[6])
-    let cheeksColor = color(genes[7], genes[8], genes[9])
-    
-    let body_scale = map(genes[0], 0, 1, 0.15, 0.75)
-    let eye_scale = map(genes[10], 0, 1, 0.35*body_scale, 0.95*body_scale)
-    let cheeks_scale = map(genes[10], 0, 1, 0.35*body_scale, 0.95*body_scale)
-
-    let eyes_type = floor(map(genes[11], 0, 1, 0, 3))
-    let cheeks_type = floor(map(genes[12], 0, 1, 0, 2))
-    
-
-    // ~ now we start drawing! ~
     
     push()
 
-    let body_w = f_body.width*body_scale
-    let body_h = f_body.height*body_scale
+    let body_w = f_body.width*this.body_scale
+    let body_h = f_body.height*this.body_scale
     
-
     // ~ adjusting the randomly generated coordinates (we want a full dango on screen) ~
     
+    let coordinatesAreOk = false
+
     // canvas borders
     if(this.x + body_w > windowWidth){
       this.x -= body_w
@@ -80,68 +73,70 @@ class Dango {
       this.x += body_w
       this.y += body_h
     }
+    coordinatesAreOk = true
+
+    if (coordinatesAreOk){
+        
+      // ='='='='='= DISPLAY_BODY ='='='='='=
+      
+      imageMode(CORNER)
+      if (this.body_scale>0.3){ 
+        tint(this.bodyColor)
+        image(f_body, this.x, this.y, body_w, body_h)
+        tint(255)
+        image(s_body, this.x, this.y, body_w, body_h)
+      }else{
+        tint(this.bodyColor)
+        image(f_body, this.x, this.y, body_w, body_h)
+        tint(255)
+        image(s_body, this.x, this.y, body_w, body_h)
+      }
 
 
-    // ='='='='='= DISPLAY_BODY ='='='='='=
-    
-    imageMode(CORNER)
-    if (body_scale>0.3){ 
-      tint(bodyColor)
-      image(f_body, this.x, this.y, body_w, body_h)
-      tint(255)
-      image(s_body, this.x, this.y, body_w, body_h)
-    }else{
-      tint(bodyColor)
-      image(f_body, this.x, this.y, body_w, body_h)
-      tint(255)
-      image(s_body, this.x, this.y, body_w, body_h)
+      // ='='='='='= DISPLAY_EYES ='='='='='=
+      
+      imageMode(CENTER)
+      tint(this.eyecolor)
+      
+      if(this.eyes_type==0){
+        image(f_eyes0, this.x+body_w/2, this.y+body_h/4, f_eyes0.width*this.eye_scale, f_eyes0.height*this.eye_scale)
+      }
+      if(this.eyes_type==1){
+        image(f_eyes1, this.x+body_w/2, this.y+body_h/4, f_eyes1.width*this.eye_scale, f_eyes0.height*this.eye_scale)
+      }
+      if(this.eyes_type==2){
+        image(f_eyes2, this.x+body_w/2, this.y+body_h/4, f_eyes2.width*this.eye_scale, f_eyes0.height*this.eye_scale)
+      }
+
+
+      // ='='='='='= DISPLAY_CHEEKS ='='='='='=
+      
+      if(this.cheeks_type==1){
+        tint(this.cheeksColor)
+        image(f_cheeks, this.x+body_w/2, this.y+(2*body_h/4), f_cheeks.width*this.cheeks_scale, f_cheeks.height*this.cheeks_scale)
+        tint (255)
+        image(s_cheeks0, this.x+body_w/2, this.y+(2*body_h/4), s_cheeks0.width*this.cheeks_scale, s_cheeks0.height*this.cheeks_scale)
+      }
+      else{
+        tint (this.cheeksColor)
+        image(s_cheeks1, this.x+body_w/2, this.y+(2*body_h/4), s_cheeks0.width*this.cheeks_scale, s_cheeks0.height*this.cheeks_scale)
+      }
+
+
+      // ='='='='='='='='= DISPLAY_THIS.COLORBOX ='='='='='='='='=
+
+      stroke(0.25)
+      fill (this.bodyColor)
+      rect(this.index*windowWidth/20, 0.873*windowHeight,  windowWidth/20, 0.045*windowHeight)
+      pop()
+
+
+      // ='='='='='='='='= DISPLAY_THIS.FITNESS_VALUE ='='='='='='='='=
+
+      textAlign(CENTER)
+      textFont('Bebas-Regular')
+      text('' + floor(this.fitness), this.index*windowWidth/20 + 0.5*windowWidth/20, 0.9*windowHeight)
     }
-
-
-    // ='='='='='= DISPLAY_EYES ='='='='='=
-    
-    imageMode(CENTER)
-    tint(eyecolor)
-    
-    if(eyes_type==0){
-      image(f_eyes0, this.x+body_w/2, this.y+body_h/4, f_eyes0.width*eye_scale, f_eyes0.height*eye_scale)
-    }
-    if(eyes_type==1){
-      image(f_eyes1, this.x+body_w/2, this.y+body_h/4, f_eyes1.width*eye_scale, f_eyes0.height*eye_scale)
-    }
-    if(eyes_type==2){
-      image(f_eyes2, this.x+body_w/2, this.y+body_h/4, f_eyes2.width*eye_scale, f_eyes0.height*eye_scale)
-    }
-
-
-    // ='='='='='= DISPLAY_CHEEKS ='='='='='=
-    
-    if(cheeks_type==1){
-      tint(cheeksColor)
-      image(f_cheeks, this.x+body_w/2, this.y+(2*body_h/4), f_cheeks.width*cheeks_scale, f_cheeks.height*cheeks_scale)
-      tint (255)
-      image(s_cheeks0, this.x+body_w/2, this.y+(2*body_h/4), s_cheeks0.width*cheeks_scale, s_cheeks0.height*cheeks_scale)
-    }
-    else{
-      tint (cheeksColor)
-      image(s_cheeks1, this.x+body_w/2, this.y+(2*body_h/4), s_cheeks0.width*cheeks_scale, s_cheeks0.height*cheeks_scale)
-    }
-
-
-    // ='='='='='='='='= DISPLAY_THIS.COLORBOX ='='='='='='='='=
-
-    stroke(0.25)
-    fill (bodyColor)
-    rect(this.index*windowWidth/20, 0.873*windowHeight,  windowWidth/20, 0.045*windowHeight)
-    pop()
-
-
-    // ='='='='='='='='= DISPLAY_THIS.FITNESS_VALUE ='='='='='='='='=
-
-    textAlign(CENTER)
-    textFont('Bebas-Regular')
-    text('' + floor(this.fitness), this.index*windowWidth/20 + 0.5*windowWidth/20, 0.9*windowHeight)
-    
   }
 
 
@@ -151,11 +146,11 @@ class Dango {
   // ='='='='='='='='='='='='='='='='='= 'GET' METHODS ='='='='='='='='='='='='='='='='='=
 
   getFitness() {
-    return this.fitness;
+    return this.fitness
   }
 
   getDNA() {
-    return this.dna;
+    return this.dna
   }
 
 }
