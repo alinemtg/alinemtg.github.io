e4f5bcbe6fe0444043d6eec2921679670dbd6b13
Igor Poncell <igorponcell@MacBook-Pro-de-Igor.local>
2019-11-28 10:16:37 -0300

    add mod logic

--- day.js
+++ day.js
@@ -24,21 +24,34 @@ class Day {
   // ='='='='='='='='='='='='='='='='='= CHANGING FITNESS ='='='='='='='='='='='='='='='='='=
 
     getHourInfluence(brightness, eyes_type, cheeks_type){
-        var influence = 0
+        var influence = 1
 
     // ='='='='='='='='= A.M. ='='='='='='='='=
 
         if (this.hour>=5 && this.hour<10){
             if (brightness>=0.3){
-                influence = 1.25
+                influence = influence * 1.25
             }else{
-                influence = 0.75
+                influence = influence *  0.75
             }
+
+            if (eyes_type == 1) {
+                influence = influence * 1.30
+            } else if (eyes_type == 2) {
+                influence = influence * 0.70
+            }
+
         } else if (this.hour>=10 && this.hour<15) {
             if (brightness>=0.3){
-                influence = 1.50
+                influence = influence *  1.50
             }else{
-                influence = 0.50
+                influence = influence * 0.50
+            }
+
+            if (eyes_type == 1) {
+                influence = influence * 1.60
+            } else if (eyes_type == 2) {
+                influence = influence * 0.40
             }
         }
 
@@ -47,22 +60,39 @@ class Day {
 
           else if (this.hour>=15 && this.hour<19) {
             if(brightness<0.4){
-                influence = 1.5
+                influence = influence *  1.5
             }else{
-                influence = 0.5
+                influence = influence * 0.5
+            }
+
+            if (eyes_type == 1) {
+                influence = influence * 0.80
+            } else if (eyes_type == 2) {
+                influence = influence * 1.20
             }
         }
         else if (this.hour>=19 && this.hour<2) {
             if(brightness<0.4){
-                influence = 1.7
+                influence = influence * 1.7
             }else{
                 influence = 0.3
             }
+
+            if (eyes_type == 1) {
+                influence = influence * 0.4
+            } else if (eyes_type == 2) {
+                influence = influence * 1.6
+            }
         } else {
             if(brightness>=0.3){
-                influence = 1.1
+                influence = influence * 0.5
             }else{
-                influence = 0.9
+                influence = influence * 0.9
+            }
+            if (eyes_type == 1) {
+                influence = influence * 0.5
+            } else if (eyes_type == 2) {
+                influence = influence * 1.8
             }
         }
         return influence
